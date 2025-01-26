b94d7c845f3b43584112dc0bef399972de4ff8ac
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-25 21:44:11 -0500

    added songs based on artist, lol

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ with open("100MostStreamedSongs.csv", encoding='utf-8') as file:
   reader=csv.DictReader(file)
   for row in reader:
     dir=os.listdir()
-    artist=row["Artist(s)"]
+    artist=row["Artist(s)"].title()
     if artist not in dir:
       os.mkdir(artist)
+    song=row["Song"]
+    path_way=os.path.join(f"{artist}/",song)
+    f=open(path_way,'w')
+    f.close()
